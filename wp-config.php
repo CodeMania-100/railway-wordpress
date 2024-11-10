d36192ee963cc9d2eb4da606721c0168aed22203
CodeMania-100 <achzaka100@gmail.com>
2024-11-10 16:48:26 +0200

    Updated added more options  wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,17 +1,33 @@
-<?php $table_prefix = getenv('WORDPRESS_TABLE_PREFIX') ?: 'wp2_';
+<?php
+// ** Database Settings ** //
+$table_prefix = getenv('WORDPRESS_TABLE_PREFIX') ?: 'wp2_';
 
 // Force these settings
-define('WORDPRESS_DB_HOST', getenv('MYSQLHOST'));
-define('WORDPRESS_DB_NAME', getenv('MYSQL_DATABASE'));
-define('WORDPRESS_DB_USER', getenv('MYSQLUSER'));
-define('WORDPRESS_DB_PASSWORD', getenv('MYSQLPASSWORD'));
+define('DB_NAME', getenv('MYSQL_DATABASE'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASSWORD', getenv('MYSQLPASSWORD'));
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 // Enable debugging
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', true);
+define('WP_DEBUG_DISPLAY', false);
+ini_set('log_errors', true);
+ini_set('error_log', '/tmp/wordpress-errors.log');
 
-// Generate these using https://api.wordpress.org/secret-key/1.1/salt/
+// Set site URLs
+define('WP_HOME', getenv('WP_HOME'));
+define('WP_SITEURL', getenv('WP_SITEURL'));
+
+// Force SSL
+define('FORCE_SSL_ADMIN', true);
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+// Authentication Keys and Salts
 define('AUTH_KEY',         getenv('AUTH_KEY'));
 define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
 define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
@@ -19,9 +35,17 @@ define('NONCE_KEY',        getenv('NONCE_KEY'));
 define('AUTH_SALT',        getenv('AUTH_SALT'));
 define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
 define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT',      getenv('NONCE_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
 
-// Add this to force SSL/HTTPS:
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
+// Disable automatic updates
+define('AUTOMATIC_UPDATER_DISABLED', true);
+define('WP_AUTO_UPDATE_CORE', false);
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
